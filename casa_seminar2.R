5d623fd9840a067ba6f54cb43282974d90d555ec
Bin Chi <bin.chi.16@ucl.ac.uk>
2020-11-24 14:46:41 +0000

    upd2

--- casa_seminar2.R
+++ casa_seminar2.R
@@ -1,5 +1,88 @@
 library(ggplot2)
 
+library("RPostgreSQL")
+#library("lme4") 
+library("lmerTest")
+library("optimx")
+#library("dplyr")
+drv <- dbDriver("PostgreSQL")
+###creates a connection to the postgres database
+### note that "con" will be used later in each connection to the database
+
+con <- dbConnect(drv, dbname = "epc",port=5432, user="postgres",password=232323)
+
+tran2<- dbGetQuery(con,"select * from ch5 where yearchi=2009")
+head(tran2)
+unique(tran2$yearchi)
+
+data1<-
+
+tran2 <- tran
+yearui <- sort(unique(tran2$yearchi))
+resutlla <- data.frame()
+yy <- 0
+
+setwd("D:/R/work/chapter2")
+
+
+mlist <-unique(tran2$laua)
+  
+  ms <- 0
+  j <- 1
+  resultp <- data.frame()
+  for(ms in mlist)
+  {
+    print(ms)
+    print(j)
+    autodata2 <- tran2[tran2$laua==ms,]
+    group<- group_by(autodata2,laua)
+    addsummary<- summarize(group,cor(price,tfarea,method="pearson"),n(),cor.test(price,tfarea,method="pearson")$conf.int[1],cor.test(price,tfarea,method="pearson")$conf.int[2],cor.test(price,tfarea,method="pearson")$p.value)
+    temp <- data.frame()
+    temp[1,"la"] <- addsummary[1,1]
+    temp[1,"corr"]<- round(addsummary[1,2], digits=2)
+    temp[1,"count"] <- addsummary[1,3]
+    temp[1,"conf1"] <- addsummary[1,4]
+    temp[1,"couf2"] <- addsummary[1,5]
+    temp[1,"pvalue"] <- addsummary[1,6]
+    resultp<- rbind( resultp,temp) 
+    j <- j+1
+    
+  }
+#E07000242
+houseprice1 <-tran2[tran2$laua=="E09000001"|tran2$laua=="E07000177",]
+unique(houseprice1$laua)
+# setwd("D:/R/tran2")
+# geo <- read.csv("geo1.csv")
+# 
+# head(geo)
+# geo1 <- sqldf("select  distinct gor,RGN11NM from geo
+#               ", drv="SQLite", dbname=":memory:" )
+# 
+# 
+# geo5 <- sqldf("select  distinct  laua,ldnm,RGN11NM,gor from geo
+#               ", drv="SQLite", dbname=":memory:" )
+getwd()
+
+houseprice1<-merge(houseprice1,geo5,by="laua")
+unique(houseprice1$laua)
+unique(houseprice1$ldnm)
+head(houseprice1)
+houseprice1<-houseprice1[,c("price","dateoftransfer","yearchi","tfarea","laua","ldnm","RGN11NM")]
+write.csv(houseprice1,"housedata1.csv")
+
+
+
+ggplot(houseprice1,aes(x=tfarea,y=price/1000))+
+  geom_point(color="#56b4e9",size = 1.2)+
+  facet_wrap(~ ldnm)+
+  scale_y_continuous(name = "Transaction Price (in £1000s)",labels = scales::comma)+
+  xlab(expression("Total floor area (" ~ m^2 ~ ")"))+
+  theme_bw()+
+  theme(axis.title = element_text(size=15),axis.text = element_text(size=13),strip.text = element_text(size=15))
+ 
+data1<-tran2[tran2$laua=="E07000242",]
+cor(data1$price,data1$tfarea, method = "pearson")
+
 # read in the first dataset
 housedata1<-read.csv("data1.csv")
 # read in the second dataset
